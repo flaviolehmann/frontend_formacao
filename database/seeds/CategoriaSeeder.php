ae5e9d2adedfe122c283064395bd12d77ce78e31
joao.calixto <joao.calixto@basis.com.br>
2020-10-20 16:22:13 -0300

    seeder de categorias feito

---
+++ database/seeds/CategoriaSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categorias')->insert([
+            'descricao' => 'entrada'
+        ]);
+        DB::table('categorias')->insert([
+            'descricao' => 'hatch pequeno'
+        ]);
+        DB::table('categorias')->insert([
+            'descricao' => 'hatch medio'
+        ]);
+        DB::table('categorias')->insert([
+            'descricao' => 'sedã medio'
+        ]);
+        DB::table('categorias')->insert([
+            'descricao' => 'sedã grande'
+        ]);
+        DB::table('categorias')->insert([
+            'descricao' => 'SUV'
+        ]);
+        DB::table('categorias')->insert([
+            'descricao' => 'pick-up'
+        ]);
+    }
+}
